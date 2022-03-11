4a390df7f6e32504a0f08ccfa197c24baa9a2ce3
Full Stack Developer <webdeveloper2009@outlook.com>
2022-03-11 07:30:27 -0800

    update-css-click-function

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./styles.css";
 
 const itemTextList = ["Op1", "Op 2", "Op 3", "123", "1234", "12345", "12334"];
 
 export default function App() {
   const [displayValue, setDisplayValue] = useState(itemTextList);
+  const inputRef = useRef(null);
+
   const handleChangeText = (e) => {
     const temp = itemTextList.filter((item) => item.includes(e.target.value));
     if (temp.length === 0) setDisplayValue(itemTextList);
     else setDisplayValue(temp);
   };
 
-  const handleItemClick = (index) => {
-    console.log("clicked");
+  const handleItemClick = (text) => {
+    inputRef.current.value = text;
+    setDisplayValue([text]);
   };
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <input className="textbox" type="text" onChange={handleChangeText} />
-      <div className="listbox">
-        {displayValue.map((text, index) => (
-          <div
-            className="filterList"
-            key={text}
-            onClick={handleItemClick(text, index)}
-          >
-            {text}
-          </div>
-        ))}
+      <div className="searchBox">
+        <input
+          className="textbox"
+          type="text"
+          ref={inputRef}
+          onChange={handleChangeText}
+        />
+        <div className="listbox">
+          {displayValue.map((text, index) => (
+            <div
+              className="filterList"
+              key={text}
+              onClick={() => handleItemClick(text)}
+            >
+              {text}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
